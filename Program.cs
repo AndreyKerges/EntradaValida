ee8a6d6bb6fd2b50ebad0ad5d1ea3e89724cf1df
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-20 17:10:15 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿int numero;
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Digite um número entre 1 e 9 (ou 0 para cancelar):");
+    numero = Convert.ToInt32(Console.ReadLine()!);
+    Console.ResetColor();
+
+    if(numero > 0)
+    {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Número selecionado: {numero}");
+    Console.WriteLine($"Volte Sempre");
+    Console.ResetColor();
+    }
+    
+    
+    if( numero == 0)
+    {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Operação Cancelada \n");
+    Console.WriteLine($"Volte Sempre");
+    Console.ResetColor();
+    }
+
+while (numero != 0)
+{
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Digite um número entre 1 e 9 (ou 0 para cancelar):");
+    numero = Convert.ToInt32(Console.ReadLine()!);
+    Console.ResetColor();
+
+
+    if(numero > 0)
+    {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Número selecionado: {numero}");
+    Console.WriteLine($"Volte Sempre");
+    Console.ResetColor();
+    }
+    
+    
+    if( numero == 0)
+    {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Operação Cancelada \n");
+    Console.WriteLine($"Volte Sempre");
+    Console.ResetColor();
+    }
+}
